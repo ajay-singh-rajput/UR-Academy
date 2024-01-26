38568a30e6a8b175a6f6ddd2c124d1e54a9c48d2
ajay-singh-rajput <ajay091.ar@gmail.com>
2024-01-26 17:40:32 +0530

    nodeMailer added

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -4,11 +4,16 @@ import { catchAsyncError } from "./catchAsyncError";
 import loginRoutes from '../routes/loginRoutes';
 import { NextFunction, Request, Response } from "express";
 
+export interface IGetUserAuthInfoRequest extends Request {
+    user: string,// or any other type
+    id: string // or any other type
+  }
+
 interface JwtPayload{
     id:string
 }
 
-export const isAuthenticated = catchAsyncError(async(req:Request, res:Response, next:NextFunction)=>{
+export const isAuthenticated = catchAsyncError(async(req:IGetUserAuthInfoRequest, res:Response, next:NextFunction)=>{
     const headerData:string = req.headers.authorization || ''
     const checkData:{id:string;token:string}={
         id:headerData.split('')[0],
--- server/src/utils/nodeMailer.ts
+++ server/src/utils/nodeMailer.ts
@@ -1,31 +1,36 @@
-// import nodeMailer from 'nodemailer';
-// import UserModel from '../models/userModel';
-// import { NextFunction, Request, Response } from 'express';
-// import { ErrorHandler } from './ErrorHandler';
+import nodeMailer from 'nodemailer';
+import UserModel from '../models/userModel';
+import { NextFunction, Request, Response } from 'express';
+import { ErrorHandler } from './ErrorHandler';
 
-// export const sendMail = (req:Request, res:Response, next:NextFunction, url:string)=>{
-//     const transport = nodeMailer.createTransport({
-//         service:'gmail',
-//         host:'smpt.gmail.com',
-//         post:465,
-//         secure:true,
-//         auth:{
-//             user:process.env.MAIL_EMAIL_ADDRESS,
-//             pass:process.env.MAIL_PASSWORD,
-//         }
-//     });
-//     const mailOption = {
-//         from:'Indian Privet Limited',
-//         to:req.body.email,
-//         subject:"Password reset link",
-//         html:`<h1>click blow link to reset password </h1><br/> <h1><a>${url}</a></h1>`
-//     };
-//     transport.sendMail(mailOption, (err, info)=>{
-//         if(err) return next(new ErrorHandler(err, 500));
-//         console.log(info);
-//         return res.status(200).json({
-//             message:"mail send successfully",
-//             url,
-//         })
-//     })
-// }
+
+export const sendMail = (req:Request, res:Response, next:NextFunction, url:string)=>{
+    const transport = nodeMailer.createTransport({
+        service:'gmail',
+        host:'smpt.gmail.com',
+        port:465,
+        secure:true,
+        auth:{
+            user:process.env.MAIL_EMAIL_ADDRESS,
+            pass:process.env.MAIL_PASSWORD,
+        }
+    });
+    const mailOption = {
+        from:'Indian Privet Limited',
+        to:req.body.email,
+        subject:"Password reset link",
+        html:`<h1>click blow link to reset password </h1><br/> <h1><a>${url}</a></h1>`
+    };
+    transport.sendMail(mailOption, (err, info)=>{
+        if(err){
+            const errorMessage = err instanceof Error ? err.message : 'unknown error';
+            console.log(err);
+            return next(new ErrorHandler(errorMessage, 500))
+        }
+        console.log(info);
+        return res.status(200).json({
+            message:"mail send successfully",
+            url,
+        })
+    })
+}
