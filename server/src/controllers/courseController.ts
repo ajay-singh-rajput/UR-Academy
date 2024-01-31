ac07f820ffc1c66da8243d3627b656045e9cf669
ajay-singh-rajput <ajay091.ar@gmail.com>
2024-01-31 21:45:07 +0530

    course handle

--- server/src/controllers/courseController.ts
+++ server/src/controllers/courseController.ts
@@ -22,7 +22,6 @@ export const createCourse = catchAsyncError( async(req:IGetUserAuthInfoRequest, 
     res.json({message:'created successfully', course:newCourse});
 });
 
-
 export const createChapter = catchAsyncError(async(req:IGetUserAuthInfoRequest, res:Response, next:NextFunction)=>{
     const course = await CourseModel.findById(req.params.id);
     if(!course)return next(new ErrorHandler('course does not present', 404));
@@ -58,6 +57,37 @@ export const createChapter = catchAsyncError(async(req:IGetUserAuthInfoRequest, 
 export const editChapter = catchAsyncError( async(req:IGetUserAuthInfoRequest, res:Response, next:NextFunction)=>{
   const course = await CourseModel.findById(req.params.courseID).exec();
   if(!course) return next(new ErrorHandler('Course does not available', 404));
-  const chapterDetails = course.chapter.findIndex((thisValue, index)=>{if(thisValue.id === req.params.chapterID){return {thisValue:thisValue, index:index}}});
-  course.chapter[chapterDetails] = 
-})
+  const chapterIndex = course.chapter.findIndex((thisValue, index)=>{if(thisValue.id === req.params.chapterID){return index}});
+  course.chapter[chapterIndex] = {
+    id:req.params.chapterID,
+    title:req.body.title,
+    description:req.body.description,
+    sourceLink:req.body.sourceLink
+  }
+  await course.save();
+  res.json({message:"Data updated successfully",chapter:course.chapter[chapterIndex]})
+});
+
+export const deleteChapter = catchAsyncError(async(req:IGetUserAuthInfoRequest, res:Response, next:NextFunction)=>{
+  const course = await CourseModel.findById(req.params.courseID).exec();
+  if(!course) return next(new ErrorHandler('Course does not available', 404));
+  const chapterIndex = course.chapter.findIndex((thisValue, index)=>{if(thisValue.id === req.params.chapterID){return index}});
+  course.chapter.slice(1,chapterIndex);
+  await course.save();
+  res.json({message:"Chapter Deleted successfully"});
+});
+
+export const editCourse = catchAsyncError(async(req:IGetUserAuthInfoRequest, res:Response, next:NextFunction)=>{
+  const course = await CourseModel.findByIdAndUpdate(req.params.courseID, req.body)
+  await course?.save();
+  res.json({message:"Course Updated successfully"});
+});
+
+export const deleteCourse = catchAsyncError(async(req:IGetUserAuthInfoRequest, res:Response, next:NextFunction)=>{
+  const course = await CourseModel.findByIdAndDelete(req.params.courseID)
+  const user = await UserModel.findById(req.id)
+  const courseIndex = user?.createdCourses.findIndex((thisValue)=>thisValue === req.params.courseID);
+  user?.createdCourses.splice(1,courseIndex);
+  await user?.save();
+  res.json({message:"Course Delete successfully"});
+});
--- server/src/routes/courseRoutes.ts
+++ server/src/routes/courseRoutes.ts
@@ -1,10 +1,18 @@
 import express, { Router } from 'express';
 import { isAuthenticated } from '../middlewares/auth';
-import { createChapter } from '../controllers/courseController';
+import { createChapter, createCourse } from '../controllers/courseController';
 import upload from '../middlewares/fileUpload';
 const router:Router = express.Router();
 
+//# POST /course/create
+router.post('/create', isAuthenticated, createCourse);
+
+//# POST /course/create
+router.post('/create', isAuthenticated, createCourse);
+
 //# POST /course/upload
 router.post('/upload', isAuthenticated, upload.single('file'), createChapter);
+
+
 
 export default router
