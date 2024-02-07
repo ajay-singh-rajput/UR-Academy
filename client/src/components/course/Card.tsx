666888ad1c86c4a72da9fc6e2656b73faf243c17
ajay-singh-rajput <ajay091.ar@gmail.com>
2024-02-07 21:40:41 +0530

    card

---
+++ client/src/components/course/Card.tsx
@@ -0,0 +1,22 @@
+import {  RiFolderVideoLine, RiStarFill, RiTimeLine } from '@remixicon/react'
+import React from 'react'
+
+const Card = (props:any) => {
+  return (
+    <>
+    <div className={`w-72 bg-[#475569]`}>
+        <img className={`w-full aspect-square`} src="https://templates.simplified.co/thumb/84b0a38d-c115-473a-b457-8a8c9071c5ad.jpg" alt="" />
+        <div className={`flex flex-col gap-2 p-3`}>
+
+        <span className={`flex w-full justify-between items-center`}> <span className='text-slate-400'>UI/UX Design</span> <span className='flex text-yellow-500'><RiStarFill/> 4.2</span></span>
+        <span>UI/UX Design for Beginners</span>
+        <span className='text-green-400'>Rs. 55</span>
+        <div className={`flex gap-3  justify-between text-slate-400`}> <span className='flex gap-2'><RiTimeLine/> 32hr 39min</span> <span className='flex gap-2'><RiFolderVideoLine/> 44 chapter</span> </div>
+        <button className={`bg-[#223243] p-3 rounded-md`}>Watch</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Card
--- client/src/components/users/profile/ProfileView.tsx
+++ client/src/components/users/profile/ProfileView.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import css from './Profile.module.css'
 import { RiImageAddLine, RiMailLine, RiMapPinUserLine, RiPhoneLine } from '@remixicon/react'
+import Card from '../../course/Card'
 
 const ProfileView = () => {
   return (
   
     <div className={`w-full h-fit md:bg-[#223243] flex   p-3`}>
-        <div className={` md:h-full w-full min-h-[50%]`}>
+        <div className={`h-full w-full min-h-[50%] flex flex-col gap-5`}>
             <div className={`relative w-fit px-32 md:w-fit md:px-10 h-fit bg-[#475569] mx-auto md:mt-2 mt-32 flex flex-col md:flex-row items-center gap-2 pt-32 md:pt-2 md:justify-center pb-5 ${css.profileInfoDiv}`}>
               <div className={`absolute md:relative w-60 h-60 rounded-full bg-red-400 mx-auto md:mx-0 md:border-[#223243]  -top-32 md:top-0  ${css.dpDiv}`}>
                 <img className={`w-full h-full object-cover object-center rounded-full`} src="https://www.sketchappsources.com/resources/source-image/profile-illustration-gunaldi-yunus.png" alt="" />
@@ -21,7 +22,12 @@ const ProfileView = () => {
               <span className={`text-[#d1e1e7ce] flex items-center gap-2`}><RiMailLine/> yourmail@gmail.com</span>
               </div>
             </div>
-            <div className={`w-full h-screen`}></div>
+            <div className={`flex items-center justify-center flex-col text-center gap-2 my-2`}>
+              <p className={`text-[#33ffff] `}>Tips</p>
+              <h3>Do you know ?</h3>
+              <p className={`text-[#d1e1e7ad] `}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum labore, ea ex consectetur quae explicabo suscipit neque alias magnam cupiditate vitae modi.</p>
+            </div>
+            <div className={`w-full h-screen flex gap-2 flex-wrap items-center justify-center`}> <Card name='ajay ji'/> <Card/> <Card/> <Card/></div>
         </div>
     </div>
     
