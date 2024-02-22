cba3f7c86767acecec3811a7fd1fe7f0c35ff38b
ajay-singh-rajput <ajay091.ar@gmail.com>
2024-02-22 15:54:32 +0530

    update server link

--- client/src/config/axios.tsx
+++ client/src/config/axios.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
  const instance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'https://ur-academy-api.onrender.com/',
     withCredentials: true,
 });
 
